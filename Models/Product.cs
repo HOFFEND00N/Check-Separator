a40c3ae2e2c7c9e1ef296a772a75ad41495028d0
Ivan Petrov <vanespetroff@gmail.com>
2020-07-19 11:52:11 +0300

    Project Created

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheckSeparatorMVC.Models
+{
+    public class Product
+    {
+        public double Price { get; set; }
+        public int Amount { get; set; }
+        public string Name { get; set; }
+
+    }
+}
