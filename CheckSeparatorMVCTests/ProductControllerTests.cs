804094e158435be2f8eeca9a70816edb1aad8e72
Ivan Petrov <vanespetroff@gmail.com>
2020-11-14 11:39:08 +0300

    some changes

--- CheckSeparatorMVCTests/ProductControllerTests.cs
+++ CheckSeparatorMVCTests/ProductControllerTests.cs
@@ -4,15 +4,29 @@ using System.Text;
 using Xunit;
 using Moq;
 using CheckSeparatorMVC.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
 
 namespace CheckSeparatorMVCTests
 {
-    public class ProductControllerTests
+    public class ProductControllerTests : IClassFixture<WebApplicationFactory<CheckSeparatorMVC.Startup>>
     {
-        [Fact]
-        public void ex()
+        private readonly WebApplicationFactory<CheckSeparatorMVC.Startup> factory;
+
+        public ProductControllerTests(WebApplicationFactory<CheckSeparatorMVC.Startup> factory)
         {
-           
+            this.factory = factory;
+        }
+        
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            var client = factory.CreateClient();
+
+            var response = client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
         }
     }
 }
